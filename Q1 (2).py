c7f3347771ae330c3f23507e93990daeee36b672
JZBALI344 <57322207+JZBALI344@users.noreply.github.com>
2019-11-03 20:12:28 +0500

    Add files via upload
    Assignment 1 python

---
+++ Q1 (2).py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+print("Twinkle Twinkle little star,\n\t How I wonder what you are!\n\t\t Up above the world so high,\n\t\t like a diamond in the sky.\nTwinkle Twinkle little star,\n\t How I wonder what you are!")
+
+
+# In[14]:
+
+
+print("Python version")
+print (sys.version)
+
+print("date")
+# In[17]:
+
+
+import datetime
+
+x = datetime.datetime.now()
+
+print(x)
+
+
+# In[19]:
+
+
+radius=float(input('enter radius'))
+area_of_circle=radius*3.142
+print(area_of_circle)
+
+
+# In[20]:
+
+
+first_name=input()
+last_name=input()
+print(last_name+' '+first_name)
+
+
+# In[26]:
+
+
+A = int(input("ENTER A VALUE "))
+B = int(input("ENTER A VALUE "))
+C = A+B
+print(C)
+
+
+# In[ ]:
+
+
+
+
